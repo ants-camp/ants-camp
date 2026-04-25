982ac2fa586903da32d5bc88d951a967c9cafc09
RoastedBear <kinim329@gmail.com>
2026-04-25 21:39:14 +0900

    fix: holdings 요청 DTO 검증 추가

--- apps/asset-service/src/main/java/io/antcamp/assetservice/application/dto/command/SellHoldingCommand.java
+++ apps/asset-service/src/main/java/io/antcamp/assetservice/application/dto/command/SellHoldingCommand.java
@@ -1,8 +1,11 @@
 package io.antcamp.assetservice.application.dto.command;
 
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
@@ -11,7 +14,13 @@ import java.util.UUID;
 @AllArgsConstructor
 public class SellHoldingCommand {
 
+    @NotNull
     private UUID accountId;
+
+    @NotBlank
     private String stockCode;
+
+    @NotNull
+    @Positive
     private Integer stockAmount;
 }
