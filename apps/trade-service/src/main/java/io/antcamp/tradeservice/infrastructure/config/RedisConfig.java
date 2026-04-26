e762f8db493ad76a6d75da35e5cee0c178ea90ec
byeongdoohan <quden04@gmail.com>
2026-04-27 02:27:06 +0900

    feat: kis websocket, rest 관련 추가

---
+++ apps/trade-service/src/main/java/io/antcamp/tradeservice/infrastructure/config/RedisConfig.java
@@ -0,0 +1,48 @@
+package io.antcamp.tradeservice.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(
+            RedisConnectionFactory connectionFactory
+    ){
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .transactionAware()
+                .build();
+    }
+}
--- apps/trade-service/src/main/java/io/antcamp/tradeservice/presentation/TradeController.java
+++ apps/trade-service/src/main/java/io/antcamp/tradeservice/presentation/TradeController.java
@@ -5,6 +5,7 @@ import io.antcamp.tradeservice.application.service.TradeService;
 import io.antcamp.tradeservice.presentation.dto.KisAccessToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TradeController {
     private final TradeService tradeService;
 
+    @PostMapping("/access-token")
     public ResponseEntity<ApiResponse<KisAccessToken>> getKisAccessToken(){
-        return ApiResponse.ok(tradeService.getAccessToken());
+        return ApiResponse.ok(tradeService.requestAccessToken());
     }
 }
