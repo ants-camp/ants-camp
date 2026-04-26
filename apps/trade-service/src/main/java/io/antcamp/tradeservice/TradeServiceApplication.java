c72013fdc98745ebc4e261c5a5840be6cf2901a6
byeongdoohan <quden04@gmail.com>
2026-04-27 02:19:32 +0900

    feat: openfeign 추가

--- apps/trade-service/src/main/java/io/antcamp/tradeservice/TradeServiceApplication.java
+++ apps/trade-service/src/main/java/io/antcamp/tradeservice/TradeServiceApplication.java
@@ -1,9 +1,12 @@
 package io.antcamp.tradeservice;
 
+import io.antcamp.tradeservice.infrastructure.client.KisApproveClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackageClasses = KisApproveClient.class)
 public class TradeServiceApplication {
 
     public static void main(String[] args) {
