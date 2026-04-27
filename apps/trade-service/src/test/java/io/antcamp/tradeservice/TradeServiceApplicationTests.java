9865ed9f66f11ac4613f217203378017a54f903a
byeongdoohan <quden04@gmail.com>
2026-04-27 12:55:39 +0900

    fix: build 실패 수정

--- apps/trade-service/src/test/java/io/antcamp/tradeservice/TradeServiceApplicationTests.java
+++ apps/trade-service/src/test/java/io/antcamp/tradeservice/TradeServiceApplicationTests.java
@@ -1,10 +1,17 @@
 package io.antcamp.tradeservice;
 
+import io.antcamp.tradeservice.infrastructure.client.KisWebSocketClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")   // application-test.yml 를 읽음
+@Import(EmbeddedRedisConfig.class)
 class TradeServiceApplicationTests {
+
+    KisWebSocketClient kisWebSocketClient;
 
     @Test
     void contextLoads() {
