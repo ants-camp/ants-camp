4f765374f786658569bec506bfb8411fbc6e50ef
byeongdoohan <quden04@gmail.com>
2026-04-27 12:23:13 +0900

    fix: build 실패 수정

--- apps/trade-service/src/main/java/io/antcamp/tradeservice/infrastructure/config/RedisConfig.java
+++ apps/trade-service/src/main/java/io/antcamp/tradeservice/infrastructure/config/RedisConfig.java
@@ -9,7 +9,9 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -24,6 +26,21 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.port}")
     private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
 
     @Bean
     public StringRedisTemplate stringRedisTemplate(
--- apps/trade-service/src/test/java/io/antcamp/tradeservice/TradeServiceApplicationTests.java
+++ apps/trade-service/src/test/java/io/antcamp/tradeservice/TradeServiceApplicationTests.java
@@ -1,9 +1,14 @@
 package io.antcamp.tradeservice;
 
+import io.antcamp.tradeservice.config.EmbeddedRedisConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(EmbeddedRedisConfig.class)
 class TradeServiceApplicationTests {
 
     @Test
