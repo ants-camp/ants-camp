0095188433abd2669658a4b38f909cdb6f1f78f1
jaeHyun <dacapo629@naver.com>
2026-04-28 15:46:22 +0900

    fix: 대회 빌드 해결

--- apps/competition-service/src/test/java/io/antcamp/competitionservice/CompetitionServiceApplicationTests.java
+++ apps/competition-service/src/test/java/io/antcamp/competitionservice/CompetitionServiceApplicationTests.java
@@ -2,15 +2,19 @@ package io.antcamp.competitionservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1, topics = {"competition.registered", "competition.finished"})
 @TestPropertySource(properties = {
         "spring.datasource.url=jdbc:h2:mem:testdb",
         "spring.datasource.driver-class-name=org.h2.Driver",
         "spring.datasource.username=sa",
         "spring.datasource.password=",
-        "spring.jpa.hibernate.ddl-auto=create-drop"
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+        "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
 })
 class CompetitionServiceApplicationTests {
     @Test
