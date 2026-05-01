ef3f084a1a7df6fa59ff13ee23b3df1620f0a697
byeongdoohan <quden04@gmail.com>
2026-05-01 14:25:47 +0900

    feat: buy 추가

--- apps/trade-service/src/main/java/io/antcamp/tradeservice/domain/model/Trade.java
+++ apps/trade-service/src/main/java/io/antcamp/tradeservice/domain/model/Trade.java
@@ -2,5 +2,30 @@ package io.antcamp.tradeservice.domain.model;
 
 import common.entity.BaseEntity;
 
-public class Trade {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record Trade(
+        UUID tradeId,
+        UUID accountId,
+        TradeType tradeType,
+        LocalDateTime tradeAt,
+        String stockCode,
+        int stockAmount,
+        TradeStatus tradeStatus,
+        double totalPrice
+) {
+
+    public static Trade create(UUID tradeId, UUID accountId, TradeType tradeType,LocalDateTime tradeAt, String stockCode, int stockAmount, double totalPrice){
+        return new Trade(
+                tradeId,
+                accountId,
+                tradeType,
+                tradeAt,
+                stockCode,
+                stockAmount,
+                TradeStatus.PENDING,
+                totalPrice
+        );
+    }
 }
--- apps/trade-service/src/main/java/io/antcamp/tradeservice/presentation/TradeController.java
+++ apps/trade-service/src/main/java/io/antcamp/tradeservice/presentation/TradeController.java
@@ -2,8 +2,11 @@ package io.antcamp.tradeservice.presentation;
 
 import common.dto.ApiResponse;
 import io.antcamp.tradeservice.application.service.TradeService;
+import io.antcamp.tradeservice.infrastructure.annotation.LoginAccount;
+import io.antcamp.tradeservice.infrastructure.annotation.LoginUser;
 import io.antcamp.tradeservice.infrastructure.dto.AccessTokenResponse;
-import io.antcamp.tradeservice.presentation.dto.KisAccessToken;
+import io.antcamp.tradeservice.presentation.dto.BuyStockRequest;
+import io.antcamp.tradeservice.presentation.dto.BuyStockResponse;
 import io.antcamp.tradeservice.presentation.dto.MinutePriceResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -11,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,11 +30,20 @@ public class TradeController {
 
     // GET /api/trades/minute-price?stock_code=005930&date_time=2024-01-01T09:00:00
     @GetMapping("/minute-price")
-    public ResponseEntity<ApiResponse<MinutePriceResponse>> getMinutePrice (
+    public ResponseEntity<ApiResponse<Double>> getMinutePrice (
             @RequestParam("stock_code") String stockCode,
             @RequestParam("date_time")
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
             LocalDateTime dateTime) {
         return ApiResponse.ok(tradeService.getMinutePrice(stockCode, dateTime));
+    }
+
+    @PostMapping("/buy")
+    public ResponseEntity<ApiResponse<BuyStockResponse>> buyStock(
+            @RequestBody String stockCode,
+            @RequestBody int stockAmount,
+            @LoginAccount UUID accountId
+    ){
+        return ApiResponse.ok(tradeService.buyStock(LocalDateTime.now(), stockCode, stockAmount, accountId));
     }
 }
