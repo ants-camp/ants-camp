46bd6b05fa873a162a04ec23248b3559be4040d4
jaeHyun <dacapo629@naver.com>
2026-05-03 01:10:56 +0900

    fix: 안 쓰는 파일 제거

--- apps/competition-service/src/main/java/io/antcamp/competitionservice/application/JoinHistoryService.java
+++
@@ -1,9 +0,0 @@
-package io.antcamp.competitionservice.application;
-
-import io.antcamp.competitionservice.application.dto.JoinCompetitionCommand;
-
-public interface JoinHistoryService {
-    void join(JoinCompetitionCommand command);
-
-    void cancel(JoinCompetitionCommand command);
-}
