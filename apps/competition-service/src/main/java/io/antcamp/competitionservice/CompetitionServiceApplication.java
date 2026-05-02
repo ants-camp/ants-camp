c95b359f7993ce7b010360698e3e33cd72bbdfb5
jaeHyun <dacapo629@naver.com>
2026-05-03 03:15:20 +0900

    feat: Eureka 클라이언트 설정 추가

--- apps/competition-service/src/main/java/io/antcamp/competitionservice/CompetitionServiceApplication.java
+++ apps/competition-service/src/main/java/io/antcamp/competitionservice/CompetitionServiceApplication.java
@@ -3,6 +3,7 @@ package io.antcamp.competitionservice;
 import java.util.Optional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableDiscoveryClient
 public class CompetitionServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CompetitionServiceApplication.class, args);
