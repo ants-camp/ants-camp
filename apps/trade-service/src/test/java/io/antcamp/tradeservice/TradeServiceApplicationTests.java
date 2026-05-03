b3c9d620d7e00029c9a8d612677726d41dd010bb
byeongdoohan <quden04@gmail.com>
2026-05-03 17:33:55 +0900

    chore: test 임시 제외

--- apps/trade-service/src/test/java/io/antcamp/tradeservice/TradeServiceApplicationTests.java
+++ apps/trade-service/src/test/java/io/antcamp/tradeservice/TradeServiceApplicationTests.java
@@ -6,15 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")   // application-test.yml 를 읽음
-@Import(EmbeddedRedisConfig.class)
-class TradeServiceApplicationTests {
-
-    KisWebSocketClient kisWebSocketClient;
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//@ActiveProfiles("test")   // application-test.yml 를 읽음
+//class TradeServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
